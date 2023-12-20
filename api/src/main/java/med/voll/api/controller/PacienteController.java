10f1f327b2a634df3772c8232649bb961c02c6fd
Alisson Vital <alisson.costa-terceiro@cea.com.br>
2023-12-20 17:52:43 -0300

    Criado uma entidade Paciente para fazer uma migration

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,28 +2,27 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("medicos")
+@RequestMapping("pacientes")
 @RequiredArgsConstructor
-public class MedicoController {
+public class PacienteController {
 
-    private final MedicoRepository medicoRepository;
+    private final PacienteRepository pacienteRepository;
 
-    @PostMapping
+    @GetMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
-        medicoRepository.save(new Medico(dados));
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        pacienteRepository.save(new Paciente(dados));
         System.out.println();
         System.out.println(dados);
     }
-
 }
